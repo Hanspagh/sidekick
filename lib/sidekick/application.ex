349928f884db702e743e62f640236d5be8c7af5a
Hans Pagh <hanspagh@gmail.com>
2021-01-22 14:05:46 +0100

    auto start as distubuted node

--- lib/sidekick/application.ex
+++ lib/sidekick/application.ex
@@ -6,6 +6,7 @@ defmodule Sidekick.Application do
   use Application
 
   def start(_type, _args) do
+    :net_kernel.start([:"main2@127.0.0.1"])
     {:ok, _node} = :slave.start('127.0.0.1', :test)
     {:ok, _node} = Sidekick.start()
 
